ac658ed5247c5697b18a4581c6c8d9e74250fefc
Italo Araujo <italoasouzat.i@gmail.com>
2022-05-18 23:27:40 -0300

    ajuste

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 let name = process.env.PROJECT_NAME;
 let pc = process.env.PC;
+let ip = process.env.IP;
 let msg = `Esse é meu projeto white label ${name}
-    Meu PC - ${pc}  
+    Meu PC - ${pc} 
+    Meu IP - ${ip}
 `;
 console.log(msg);
